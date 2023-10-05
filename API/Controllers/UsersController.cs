e9c5a3cf72f608ce2b80d730c71de2f2a3277c49
Rafid Haroon <rafid_haroon@hotmail.com>
2023-10-05 23:41:40 +0100

    1st commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id}")] //api/user/2
+    public ActionResult<AppUser> GetUsers(int id)
+    {
+        return _context.Users.Find(id);
+         
+    }
+}
